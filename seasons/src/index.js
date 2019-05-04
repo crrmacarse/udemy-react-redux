b7b54b89fdcf24d043e6785cef39d262ebfc3380
crrmacarse <crrmacarse@gmail.com>
2019-05-05 00:44:47 +0800

    Messing around

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
 
-    return <div>Hi there!</div>
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            location: null,
+            text: "H",
+            errorMessage: ""
+        }
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({location: position.coords}),
+            err => this.setState({errorMessage: err.message})
+            )
+     }
+
+    changeText = () => {
+       
+        this.setState( prevState => ({
+            text: prevState.text + "H"
+        }))
+    }
+    
+    render() {
+        const {location, text, errorMessage} = this.state;
+
+        return (
+            <div>
+                <h1>{text}</h1>
+                <h1 style = {{color: 'red'}}>{errorMessage}</h1>
+                <button type = "button" onClick = {this.changeText}>click</button>
+                <p> Latitude: {location && location.latitude} </p>
+                <p> Longitude: {location && location.longitude} </p>
+            </div>);
+    }
 }
 
 ReactDOM.render(
