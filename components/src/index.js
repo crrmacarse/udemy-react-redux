1b4fb0d16cbc53bcbc3e09e5cae7549a47980df6
crrmacarse <crrmacarse@gmail.com>
2019-05-04 23:31:24 +0800

    Added seasons project

--- components/src/index.js
+++ components/src/index.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import faker from 'faker';
+
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className = "ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                Are you sure about this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar = {faker.image.avatar()} 
+                    timestamp = "May 03, 2019" 
+                    author = "Alex"
+                    content = "Lorem Ipmsum Dolor"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar = {faker.image.avatar()} 
+                    timestamp = "May 03, 2019" 
+                    author = "Jane"
+                    content = "I like this subject"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar = {faker.image.avatar()} 
+                    timestamp = "May 03, 2019" 
+                    author = "Joshua"
+                    content = "I like this writing"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar = {faker.image.avatar()} 
+                    timestamp = "May 03, 2019" 
+                    author = "Joseph"
+                    content = "Sample data only"
+                />
+            </ApprovalCard>
         </div>
     );
 };
